2d3ef9367ac17e4b79e0385a16770e896f31fb5c
ascsl <jose@ascsl.com>
2024-06-13 19:08:34 +0200

    Ejercicio Blazor Modo Auto

---
+++ tareasJLauto/tareasJLauto.Client/Shared/TodoItem.cs
@@ -0,0 +1,25 @@
+﻿namespace tareasJLauto.Shared;
+
+public enum TodoStatus
+{
+    Planificada,
+    Iniciada,
+    EnCurso,
+    Completada
+}
+
+public class TodoItem
+{
+
+    public TodoItem(string descr)
+    {
+        Title = descr;
+        Status = TodoStatus.Planificada;
+        IsBlocked = false;
+    }
+
+    public string? Title { get; set; }
+    public TodoStatus Status { get; set; }
+    public bool IsBlocked { get; set; }
+    public bool IsSelected { get; set; }
+}
---
+++ tareasJLauto/tareasJLauto/Program.cs
@@ -0,0 +1,36 @@
+//using tareasJLauto.Client.Pages;
+using tareasJLauto.Components;
+//using tareasJLauto.Shared;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(tareasJLauto.Client._Imports).Assembly);
+
+app.Run();
